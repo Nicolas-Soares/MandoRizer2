91e9130cdf6757354dcc420a5e0daff4e4cba9b0
Nicolas Soares <nicksoares3658@gmail.com>
2024-05-01 20:02:48 -0300

    wip: set loop for invalid options

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /*
 	- App vai ter várias opções
-		-- Escolher numero aleatório entre X e Y de acordo com valores inputados pelo user
-		-- Escolher nome aleatorio em lista fornecida pelo user
-		-- Escolher numero aleatorio dentre numeros fornecidos pelo user
+		-- 1 Escolher numero aleatório entre X e Y de acordo com valores inputados pelo user
+		-- 2 Escolher nome aleatorio em lista fornecida pelo user
+		-- 3 Escolher numero aleatorio dentre numeros fornecidos pelo user
 
 	- Ter opção de dar exit no app em qualquer momento apertando alguma tecla especifica
 	- Ter opção de voltar ao menu do app em qualquer momento apertando alguma tecla especifica
@@ -24,6 +24,34 @@ void printAppTitle() {
 
 int main() {
 	printAppTitle();
+
+	int optionSelected{};
 	
+	while (optionSelected == 0) {
+		std::cout << R"(
+	Select an option:
+	1 - Gan
+	2 - Gar
+	3 - GarandThumb
+	)";
+
+		std::cin >> optionSelected;
+		switch (optionSelected) {
+			case 1:
+				// Go to option
+				break;
+			case 2:
+				// Go to option
+				break;
+			case 3:
+				// Go to option
+				break;
+			default:
+				std::cout << "-- INVALID OPTION --";
+				optionSelected = 0;
+				break;
+		}
+	}
+
 	return 0;
 }
